502badb9f42929e7b7ad6fe6258e51451b8cce8e
daksh677 <121499299+daksh677@users.noreply.github.com>
2023-06-29 22:06:16 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  theme: {
+    extend: {
+      colors:{
+        primarycolor:"#8873ef",
+        headingcolor:"#081e21",
+        smallTextcolor:"#193256",
+      },
+    },
+  },
+  plugins: [],
+}
+
